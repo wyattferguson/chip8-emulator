b5422ec0abaf76c8d75abb45e05b4f8519ca257e
Wyatt <wyattf@gmail.com>
2023-05-30 15:08:30 -0400

    keypresses and more code cleanup

--- screen.py
+++ screen.py
@@ -1,8 +1,7 @@
 
 import pygame as pg
 
-from config import (BLACK, DISPLAY_SCALER, FPS, SCREEN_HEIGHT, SCREEN_WIDTH,
-                    WHITE)
+from config import BLACK, DISPLAY_SCALER, FPS, SCREEN_HEIGHT, SCREEN_WIDTH, WHITE
 
 
 class ChipScreen():
@@ -11,36 +10,46 @@ class ChipScreen():
         pg.init()
         pg.display.set_caption("Chip8 Emulator")
 
+        self.keys = "1234QWERASDFZXCV"
+        self.key_codes = [pg.key.key_code(key) for key in self.keys]
+        self.pressed_keys = [False] * 16
         self.screen_size = (SCREEN_WIDTH * DISPLAY_SCALER, SCREEN_HEIGHT * DISPLAY_SCALER)
         self.screen = pg.display.set_mode(self.screen_size)
         self.clock = pg.time.Clock()
         self.tick_speed = FPS
-        self.running = True
 
     def update(self):
-        # self.should_quit()
+        self.keyboard()
         pg.display.update()
         self.clock.tick(self.tick_speed)
 
     def clear_screen(self):
+        """black out entire screen"""
         self.screen.fill(BLACK)
 
-    def flip(self,px,py):
-        color = self.screen.get_at((px * DISPLAY_SCALER, py * DISPLAY_SCALER))
+    def flip(self, px, py):
+        """Flip color of given pixel BLACK/WHITE"""
+        scaled_pixel = (px * DISPLAY_SCALER, py * DISPLAY_SCALER)
+        color = self.screen.get_at(scaled_pixel)
+        new_color = BLACK if color == WHITE else WHITE
 
-        if color == WHITE:
-            pg.draw.rect(
-                self.screen,
-                BLACK,
-                pg.Rect(px * DISPLAY_SCALER, py * DISPLAY_SCALER, DISPLAY_SCALER, DISPLAY_SCALER),
-            )
-            return True
+        pg.draw.rect(
+            self.screen,
+            new_color,
+            pg.Rect(*scaled_pixel, DISPLAY_SCALER, DISPLAY_SCALER),
+        )
 
-        else:
-            pg.draw.rect(
-                self.screen,
-                WHITE,
-                pg.Rect(px * DISPLAY_SCALER, py * DISPLAY_SCALER, DISPLAY_SCALER, DISPLAY_SCALER),
-            )
-        return False
+        return color == WHITE
 
+    def keyboard(self):
+        """handle keypresses and quit events"""
+        for event in pg.event.get():
+
+            if event.type == pg.KEYDOWN or event.type == pg.KEYUP:
+                if event.key in self.key_codes:
+                    idx = self.key_codes.index(event.key)
+                    self.pressed_keys[idx] = not self.pressed_keys[idx]
+
+            if event.type == pg.QUIT:
+                pg.quit()
+                exit()
