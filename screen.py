d49dab4a67ec86719545f39e79fae177024f35fc
Wyatt Ferguson <wyattferguson@users.noreply.github.com>
2023-05-31 20:22:38 +0000

    Update screen.py

--- screen.py
+++ screen.py
@@ -1,8 +1,7 @@
 
 import pygame as pg
 
-from config import (BLACK, DISPLAY_SCALER, FPS, SCREEN_HEIGHT, SCREEN_WIDTH,
-                    WHITE)
+from config import BLACK, DISPLAY_SCALER, SCREEN_HEIGHT, SCREEN_WIDTH, WHITE
 
 
 class ChipScreen():
@@ -16,28 +15,25 @@ class ChipScreen():
         self.pressed_keys = [False] * 16
         self.screen_size = (SCREEN_WIDTH * DISPLAY_SCALER, SCREEN_HEIGHT * DISPLAY_SCALER)
         self.screen = pg.display.set_mode(self.screen_size)
-        self.clock = pg.time.Clock()
-        self.tick_speed = FPS
 
     def update(self):
         self.keyboard()
         pg.display.update()
-        self.clock.tick(self.tick_speed)
 
     def clear_screen(self):
         """black out entire screen"""
         self.screen.fill(BLACK)
 
-    def flip(self, px, py):
+    def flip(self, px: int, py: int):
         """Flip color of given pixel BLACK/WHITE"""
-        scaled_pixel = (px * DISPLAY_SCALER, py * DISPLAY_SCALER)
-        color = self.screen.get_at(scaled_pixel)
+        scaled_pos = (px * DISPLAY_SCALER, py * DISPLAY_SCALER)
+        color = self.screen.get_at(scaled_pos)
         swapped_color = BLACK if color == WHITE else WHITE
 
         pg.draw.rect(
             self.screen,
             swapped_color,
-            pg.Rect(*scaled_pixel, DISPLAY_SCALER, DISPLAY_SCALER),
+            pg.Rect(*scaled_pos, DISPLAY_SCALER, DISPLAY_SCALER),
         )
 
         return color == WHITE
@@ -45,7 +41,6 @@ class ChipScreen():
     def keyboard(self):
         """handle keypresses and quit events"""
         for event in pg.event.get():
-
             if event.type == pg.KEYDOWN or event.type == pg.KEYUP:
                 if event.key in self.key_codes:
                     key_idx = self.key_codes.index(event.key)
